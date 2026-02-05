857e541f6103981aeff82bd37eab0c842a3f8dfd
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2026-02-05 13:57:00 +0100

    style(ui): modificar tamaño de fuente e icono del componente Badge

--- src/components/ui/Badge.jsx
+++ src/components/ui/Badge.jsx
@@ -1,6 +1,6 @@
 export const Badge = ({ icon: Icon, text }) => (
-  <span className="inline-flex items-center px-3 py-1 rounded-full text-xs font-medium bg-slate-900 border border-slate-800 text-indigo-300">
-    {Icon && <Icon className="size-4 mr-2" />}
+  <span className="inline-flex items-center px-3 py-1 rounded-full text-[13px] font-medium bg-slate-900 border border-slate-800 text-indigo-300">
+    {Icon && <Icon className="size-4.5 mr-2" />}
     {text}
   </span>
 );
